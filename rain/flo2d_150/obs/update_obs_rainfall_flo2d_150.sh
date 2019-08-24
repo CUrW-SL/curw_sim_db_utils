447f8c11f9a88f11b02c0dca65841e43e53a6545
shadhini <jshadhiniaseka@gmail.com>
2019-08-25 00:47:17 +0530

    reorganize script to update input obs rainfall for flo2d 150

--- rain/flo2d_150/obs/update_obs_rainfall_flo2d_150.sh
+++ rain/flo2d_150/obs/update_obs_rainfall_flo2d_150.sh
@@ -2,8 +2,8 @@
 
 echo `date`
 
-echo "Changing into ~/curw_sim_utils"
-cd /home/uwcc-admin/curw_sim_utils
+echo "Changing into ~/curw_sim_db_utils"
+cd /home/uwcc-admin/curw_sim_db_utils
 echo "Inside `pwd`"
 
 
@@ -19,7 +19,7 @@ echo "Activating venv python3 virtual environment."
 source venv/bin/activate
 
 # Install dependencies using pip.
-if [ ! -f "curw_sim_obs_flo2d_150.log" ]
+if [ ! -f "curw_sim_utils.log" ]
 then
     echo "Installing PyMySQL"
     pip install PyMySQL
@@ -27,12 +27,13 @@ then
     pip install PyYAML
     echo "Installing db adapter"
     pip install git+https://github.com/shadhini/curw_db_adapter.git
+    touch curw_sim_utils.log
 fi
 
 
 # Update obs data in curw_sim for flo2d grids
 echo "Running update_obs_rainfall_flo2d_150.py"
-python update_obs_rainfall_flo2d_150.py >> curw_sim_obs_flo2d_150.log 2>&1
+python rain/flo2d_150/obs/update_obs_rainfall_flo2d_150.py >> rain/flo2d_150/obs/curw_sim_obs_flo2d_150.log 2>&1
 
 # Deactivating virtual environment
 echo "Deactivating virtual environment"
