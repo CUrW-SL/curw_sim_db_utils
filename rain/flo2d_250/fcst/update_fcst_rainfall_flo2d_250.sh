6b915f445cbb269aaaecbbad625d35f13aea3804
shadhini <jshadhiniaseka@gmail.com>
2019-08-25 00:24:52 +0530

    reorganize script to update input fcst rainfall for flo2d 250

--- rain/flo2d_250/fcst/update_fcst_rainfall_flo2d_250.sh
+++ rain/flo2d_250/fcst/update_fcst_rainfall_flo2d_250.sh
@@ -2,8 +2,8 @@
 
 echo `date`
 
-echo "Changing into ~/curw_sim_utils"
-cd /home/uwcc-admin/curw_sim_utils
+echo "Changing into ~/curw_sim_db_utils"
+cd /home/uwcc-admin/curw_sim_db_utils
 echo "Inside `pwd`"
 
 
@@ -19,7 +19,7 @@ echo "Activating venv python3 virtual environment."
 source venv/bin/activate
 
 # Install dependencies using pip.
-if [ ! -f "curw_sim_fcst_flo2d_250.log" ]
+if [ ! -f "curw_sim_utils.log" ]
 then
     echo "Installing PyMySQL"
     pip install PyMySQL
@@ -27,12 +27,13 @@ then
     pip install PyYAML
     echo "Installing db adapter"
     pip install git+https://github.com/shadhini/curw_db_adapter.git
+    touch curw_sim_utils.log
 fi
 
 
 # Update fcst data in curw_sim for flo2d grids
 echo "Running update_obs_rainfall_flo2d_250.py"
-python update_fcst_rainfall_flo2d_250.py >> curw_sim_fcst_flo2d_250.log 2>&1
+python rain/flo2d_250/fcst/update_fcst_rainfall_flo2d_250.py >> rain/flo2d_250/fcst/curw_sim_fcst_flo2d_250.log 2>&1
 
 # Deactivating virtual environment
 echo "Deactivating virtual environment"
