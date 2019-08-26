bc62bf3abae6b419aeafbeae53772f2dc65f9690
shadhini <jshadhiniaseka@gmail.com>
2019-08-26 17:09:58 +0530

    add utils to populate curw_sim with estimated discharge timeseries

--- discharge/flo2d/estimated/update_estimated_discharge.py
+++ discharge/flo2d/estimated/update_estimated_discharge.py
@@ -19,47 +19,48 @@ Update rainfall observations for flo2d models
 :param timeseries: list of [time, value] pairs
 :return:
 """
+if __name__=="__main__":
 
-try:
+    try:
 
-    curw_sim_pool = get_Pool(host=CURW_SIM_HOST, user=CURW_SIM_USERNAME, password=CURW_SIM_PASSWORD,
-            port=CURW_SIM_PORT, db=CURW_SIM_DATABASE)
+        curw_sim_pool = get_Pool(host=CURW_SIM_HOST, user=CURW_SIM_USERNAME, password=CURW_SIM_PASSWORD,
+                port=CURW_SIM_PORT, db=CURW_SIM_DATABASE)
 
-    TS = Timeseries(pool=curw_sim_pool)
+        TS = Timeseries(pool=curw_sim_pool)
 
-    method = MethodEnum.SF
+        method = MethodEnum.SF
 
-    # [station_name,latitude,longitude,target]
-    extract_stations = read_csv('grids/discharge_stations/extract_stations.csv')
-    extract_stations_dict = { }  # keys: station_name , value: [latitude, longitude, target_method]
+        # [station_name,latitude,longitude,target]
+        extract_stations = read_csv('grids/discharge_stations/extract_stations.csv')
+        extract_stations_dict = { }  # keys: station_name , value: [latitude, longitude, target_method]
 
-    for obs_index in range(len(extract_stations)):
-        extract_stations_dict[extract_stations[obs_index][0]] = [extract_stations[obs_index][1],
-                                                                 extract_stations[obs_index][2],
-                                                                 extract_stations[obs_index][3]]
+        for obs_index in range(len(extract_stations)):
+            extract_stations_dict[extract_stations[obs_index][0]] = [extract_stations[obs_index][1],
+                                                                     extract_stations[obs_index][2],
+                                                                     extract_stations[obs_index][3]]
 
-    for station_name in extract_stations_dict.keys():
-        meta_data = {
-            'latitude': float('%.6f' % float(extract_stations_dict.get(station_name)[0])),
-            'longitude': float('%.6f' % float(extract_stations_dict.get(station_name)[1])),
-            'model': extract_stations_dict.get(station_name)[2], 'method': method,
-            'grid_id': 'discharge_{}'.format(station_name)
-        }
+        for station_name in extract_stations_dict.keys():
+            meta_data = {
+                'latitude': float('%.6f' % float(extract_stations_dict.get(station_name)[0])),
+                'longitude': float('%.6f' % float(extract_stations_dict.get(station_name)[1])),
+                'model': extract_stations_dict.get(station_name)[2], 'method': method,
+                'grid_id': 'discharge_{}'.format(station_name)
+            }
 
-        tms_id = TS.get_timeseries_id_if_exists(meta_data=meta_data)
+            tms_id = TS.get_timeseries_id_if_exists(meta_data=meta_data)
 
-        if tms_id is None:
-            tms_id = TS.generate_timeseries_id(meta_data=meta_data)
-            meta_data['id'] = tms_id
-            TS.insert_run(meta_data=meta_data)
+            if tms_id is None:
+                tms_id = TS.generate_timeseries_id(meta_data=meta_data)
+                meta_data['id'] = tms_id
+                TS.insert_run(meta_data=meta_data)
 
-        timeseries = read_csv('discharge/flo2d/estimated/{}.csv'.format(station_name))
+            timeseries = read_csv('discharge/flo2d/estimated/{}.csv'.format(station_name))
 
-        if timeseries is not None and len(timeseries) > 0:
-            TS.insert_data(timeseries=timeseries, tms_id=tms_id, upsert=True)
+            if timeseries is not None and len(timeseries) > 0:
+                TS.insert_data(timeseries=timeseries, tms_id=tms_id, upsert=True)
 
-except Exception as e:
-    traceback.print_exc()
-    logger.error("Exception occurred while updating obs rainfalls in curw_sim.")
-finally:
-    destroy_Pool(pool=curw_sim_pool)
+    except Exception as e:
+        traceback.print_exc()
+        logger.error("Exception occurred while updating obs rainfalls in curw_sim.")
+    finally:
+        destroy_Pool(pool=curw_sim_pool)
