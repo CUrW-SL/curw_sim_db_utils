44d95b09e8afa3a2d0da3ee77f0589188f481173
shadhini <jshadhiniaseka@gmail.com>
2019-09-07 12:35:39 +0530

    change scipts used to update estimated discharge and waterlevel to update obs_end field with the latest datetime entry of the estimated timeseries

--- waterlevel/flo2d/estimated/update_hanwella_hourly_estimated_wl.py
+++ waterlevel/flo2d/estimated/update_hanwella_hourly_estimated_wl.py
@@ -93,6 +93,7 @@ if __name__ == "__main__":
 
         if hanwella_ts is not None and len(hanwella_ts) > 0:
             TS.insert_data(timeseries=hanwella_ts, tms_id=tms_id, upsert=True)
+            TS.update_latest_obs(id_=tms_id, obs_end=hanwella_ts[-1][1])
 
     except Exception as e:
         traceback.print_exc()
