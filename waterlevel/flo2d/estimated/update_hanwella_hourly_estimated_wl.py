2de3aeaff97436561a8810eb5bf33c13ea0db00d
shadhini <jshadhiniaseka@gmail.com>
2019-09-18 11:37:59 +0530

    change obs data source from curw database to curw_obs for obs waterlevel updating scripts and integrate filling missing mechanism to obs waterlevel updating scripts to avoid mixed timeserirs with obs and fcsts

--- waterlevel/flo2d/estimated/update_hanwella_hourly_estimated_wl.py
+++ waterlevel/flo2d/estimated/update_hanwella_hourly_estimated_wl.py
@@ -6,27 +6,22 @@ from datetime import datetime, timedelta
 from db_adapter.csv_utils import read_csv
 from db_adapter.base import get_Pool, destroy_Pool
 from db_adapter.constants import CURW_SIM_DATABASE, CURW_SIM_PASSWORD, CURW_SIM_USERNAME, CURW_SIM_PORT, CURW_SIM_HOST
-from db_adapter.constants import COMMON_DATE_TIME_FORMAT
+from db_adapter.constants import CURW_OBS_DATABASE, CURW_OBS_PORT, CURW_OBS_PASSWORD, CURW_OBS_USERNAME, CURW_OBS_HOST
 from db_adapter.curw_sim.timeseries.waterlevel import Timeseries
 from db_adapter.curw_sim.timeseries import MethodEnum
+from db_adapter.curw_sim.common import fill_ts_missing_entries
 from db_adapter.logger import logger
 
-
-TEMP_HOST = "10.138.0.6"
-TEMP_USER = "root"
-TEMP_PASSWORD = "cfcwm07"
-TEMP_DB ="curw"
-TEMP_PORT = 3306
-
+RANWALA_WL_ID = 100047
 
 if __name__ == "__main__":
 
     try:
 
-        curw_pool = get_Pool(host=TEMP_HOST, user=TEMP_USER, password=TEMP_PASSWORD,
-                port=TEMP_PORT, db=TEMP_DB)
+        curw_obs_pool = get_Pool(host=CURW_OBS_HOST, user=CURW_OBS_USERNAME, password=CURW_OBS_PASSWORD,
+                port=CURW_OBS_PORT, db=CURW_OBS_DATABASE)
 
-        connection = curw_pool.connection()
+        connection = curw_obs_pool.connection()
 
         curw_sim_pool = get_Pool(host=CURW_SIM_HOST, user=CURW_SIM_USERNAME, password=CURW_SIM_PASSWORD,
                                  port=CURW_SIM_PORT, db=CURW_SIM_DATABASE)
@@ -56,26 +51,26 @@ if __name__ == "__main__":
         tms_id = TS.get_timeseries_id_if_exists(meta_data=meta_data)
 
         ranwala_ts = []
-        hanwella_ts = []
-        start_date = (datetime.now() - timedelta(days=10)).strftime(COMMON_DATE_TIME_FORMAT)
-        end_date = (datetime.now().strftime(COMMON_DATE_TIME_FORMAT))
+        start_date = (datetime.now() - timedelta(days=10)).strftime("%Y-%m-%d %H:00:00")
+        end_date = (datetime.now() + timedelta(hours=5, minutes=30)).strftime("%Y-%m-%d %H:00:00")
 
         if tms_id is None:
             tms_id = TS.generate_timeseries_id(meta_data=meta_data)
             meta_data['id'] = tms_id
             TS.insert_run(meta_data=meta_data)
-            with connection.cursor() as cursor1:
-                cursor1.callproc('getWL', ('curw_wl_ranwala', start_date, end_date))
-                results = cursor1.fetchall()
-
-                for result in results:
-                    ranwala_ts.append([result.get('time'), result.get('value')])
         else:
-            with connection.cursor() as cursor1:
-                cursor1.callproc('getLatest24hrWaterLevel', ('curw_wl_ranwala',))
-                results = cursor1.fetchall()
-                for result in results:
-                    ranwala_ts.append([result.get('time'), result.get('value')])
+            start_date = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d %H:00:00")
+
+        with connection.cursor() as cursor1:
+            cursor1.callproc('getWL', (RANWALA_WL_ID, start_date, end_date))
+            results = cursor1.fetchall()
+            for result in results:
+                ranwala_ts.append([result.get('time'), result.get('value')])
+
+        interpolated_ranwala_ts = fill_ts_missing_entries(start=start_date, end=end_date, timeseries=ranwala_ts,
+                                                interpolation_method='linear', timestep=60)
+
+        hanwella_ts = []
 
         for i in range(len(ranwala_ts) - 1):
             # x = Ranwala
@@ -100,7 +95,7 @@ if __name__ == "__main__":
         logger.error("Exception occurred.")
     finally:
         connection.close()
-        destroy_Pool(pool=curw_pool)
+        destroy_Pool(pool=curw_obs_pool)
         destroy_Pool(pool=curw_sim_pool)
 
 
