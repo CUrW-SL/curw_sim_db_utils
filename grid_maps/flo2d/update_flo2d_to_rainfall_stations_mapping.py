7bcc183b32a42d011f1aad24a83085cf4f7fa0fd
shadhini <jshadhiniaseka@gmail.com>
2019-09-26 17:23:25 +0530

    update scripts used to populate observed timeseries for flo2d grids to avoid extracting data from observational stations situated in more than 15 km radius

--- grid_maps/flo2d/update_flo2d_to_rainfall_stations_mapping.py
+++ grid_maps/flo2d/update_flo2d_to_rainfall_stations_mapping.py
@@ -70,14 +70,17 @@ def find_nearest_obs_stations_for_flo2d_stations(flo2d_stations_csv, obs_station
             else:
                 distance = 6371 * acos(1)
 
-            distances[obs_stations[obs_index][2]] = distance
+            distances[obs_stations[obs_index][1]] = distance
 
         sorted_distances = collections.OrderedDict(sorted(distances.items(), key=operator.itemgetter(1))[:10])
 
         count = 0
         for key in sorted_distances.keys():
-            if count < 3 and sorted_distances.get(key) < 15:
+            if count < 3 and sorted_distances.get(key) <= 15:
                 flo2d_obs_mapping.extend([key, sorted_distances.get(key)])
+                count += 1
+            elif count < 3:
+                flo2d_obs_mapping.extend([-1, -1])
                 count += 1
 
         # print(flo2d_obs_mapping)
