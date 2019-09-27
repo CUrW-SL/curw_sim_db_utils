e5807c733d557d3d46eabe2d665d3771d54f78ae
shadhini <jshadhiniaseka@gmail.com>
2019-09-27 21:16:12 +0530

    add scripts to update observed rainfall for flo2d 150 grids

--- rain/flo2d_150/obs/update_obs_rainfall_flo2d_150.py
+++ rain/flo2d_150/obs/update_obs_rainfall_flo2d_150.py
@@ -4,8 +4,13 @@ from datetime import datetime, timedelta
 from db_adapter.curw_sim.constants import FLO2D_150
 from db_adapter.curw_sim.grids import GridInterpolationEnum
 from db_adapter.curw_sim.timeseries import MethodEnum
-from db_adapter.curw_sim.flo2d import update_rainfall_obs
 
+# from rain.flo2d.flo2d_common import update_rainfall_obs
+
+import sys
+sys.path.insert(0, '/home/uwcc-admin/curw_sim_db_utils/rain/flo2d')
+
+import flo2d_common
 
 if __name__=="__main__":
     try:
@@ -13,7 +18,7 @@ if __name__=="__main__":
         grid_interpolation = GridInterpolationEnum.getAbbreviation(GridInterpolationEnum.MDPA)
 
         print("{} : ####### Insert obs rainfall for FLO2D 150 grids".format(datetime.now()))
-        update_rainfall_obs(flo2d_model=FLO2D_150, method=method, grid_interpolation=grid_interpolation, timestep=15)
+        flo2d_common.update_rainfall_obs(flo2d_model=FLO2D_150, method=method, grid_interpolation=grid_interpolation, timestep=15)
 
     except Exception as e:
         traceback.print_exc()
