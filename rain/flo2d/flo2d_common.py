49732db3cdd9a270e3f0d148c8fd2220aaf8d282
shadhini <jshadhiniaseka@gmail.com>
2019-09-27 21:48:18 +0530

    debug issues in scripts used for observed timeseries updation for flo2d grids

--- rain/flo2d/flo2d_common.py
+++ rain/flo2d/flo2d_common.py
@@ -194,11 +194,9 @@ def update_rainfall_obs(flo2d_model, method, grid_interpolation, timestep):
 
             if timestep == 5:
                 if obs1_station_id != str(-1):
-                    print("obs1 station id", obs1_station_id)
                     obs1_hash_id = stations_dict_for_obs.get(obs1_station_id)
-                    print("obs1 hash id", obs1_hash_id)
+
                     ts = extract_obs_rain_5_min_ts(connection=curw_obs_connection, start_time=obs_start, id=obs1_hash_id)
-                    print("::::", ts)
                     if ts is not None and len(ts) > 1:
                         obs_timeseries.extend(process_5_min_ts(newly_extracted_timeseries=ts, expected_start=obs_start)[1:])
                         # obs_start = ts[-1][0]
