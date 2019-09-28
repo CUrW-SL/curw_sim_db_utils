1ba8fd39e6e73c43dca28025299c99f9ab9bfe0f
shadhini <jshadhiniaseka@gmail.com>
2019-09-29 01:26:46 +0530

    debug skipping flo2d grids issue while populating rain

--- rain/flo2d/flo2d_common.py
+++ rain/flo2d/flo2d_common.py
@@ -163,9 +163,7 @@ def update_rainfall_obs(flo2d_model, method, grid_interpolation, timestep):
 
         flo2d_obs_mapping = get_flo2d_cells_to_obs_grid_mappings(pool=curw_sim_pool, grid_interpolation=grid_interpolation, flo2d_model=flo2d_model)
 
-        print("{} flo2d grids, {} grid mappings".format(len(flo2d_grids), len(flo2d_obs_mapping)))
-
-        for flo2d_index in range(30000, len(flo2d_grids)):  ################## debug
+        for flo2d_index in range(len(flo2d_grids)):
             obs_start = OBS_START
             lat = flo2d_grids[flo2d_index][2]
             lon = flo2d_grids[flo2d_index][1]
@@ -175,8 +173,6 @@ def update_rainfall_obs(flo2d_model, method, grid_interpolation, timestep):
                     'model': flo2d_model, 'method': method,
                     'grid_id': '{}_{}_{}'.format(flo2d_model, grid_interpolation, (str(cell_id)).zfill(10))
                     }
-
-            print('**** grid_id,', meta_data.get('grid_id'))
 
             tms_id = TS.get_timeseries_id(grid_id=meta_data.get('grid_id'), method=meta_data.get('method'))
 
@@ -231,13 +227,9 @@ def update_rainfall_obs(flo2d_model, method, grid_interpolation, timestep):
                                 obs_timeseries.extend(process_5_min_ts(newly_extracted_timeseries=ts3, expected_start=expected_start)[1:])
             elif timestep == 15:
                 if obs1_station_id != str(-1):
-                    print('obs1 station id', obs1_station_id)
                     obs1_hash_id = stations_dict_for_obs.get(obs1_station_id)
-                    print('obs1 hash id', obs1_hash_id)
-                    print('obs start', obs_start)
 
                     ts = extract_obs_rain_15_min_ts(connection=curw_obs_connection, start_time=obs_start, id=obs1_hash_id)
-                    print('ts,', ts)
                     if ts is not None and len(ts) > 1:
                         obs_timeseries.extend(process_15_min_ts(newly_extracted_timeseries=ts, expected_start=obs_start)[1:])
                         # obs_start = ts[-1][0]
@@ -267,7 +259,6 @@ def update_rainfall_obs(flo2d_model, method, grid_interpolation, timestep):
                                     expected_start = obs_start
                                 obs_timeseries.extend(process_15_min_ts(newly_extracted_timeseries=ts3, expected_start=expected_start)[1:])
 
-            print('obs_timeseries', obs_timeseries)
 
             for i in range(len(obs_timeseries)):
                 if obs_timeseries[i][1] == -99999:
