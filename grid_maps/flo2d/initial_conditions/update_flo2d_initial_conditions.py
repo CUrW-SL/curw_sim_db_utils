3d4d203d177ab9a57ff0e1d1c0ee6810475d5134
shadhini <jshadhiniaseka@gmail.com>
2019-09-29 13:53:24 +0530

    configure scripts to remove existing initial conditions before adding new ones

--- grid_maps/flo2d/initial_conditions/update_flo2d_initial_conditions.py
+++ grid_maps/flo2d/initial_conditions/update_flo2d_initial_conditions.py
@@ -1,8 +1,11 @@
 import traceback
 from db_adapter.base import get_Pool, destroy_Pool
-from db_adapter.curw_sim.grids import add_flo2d_initial_conditions, get_flo2d_initial_conditions
+from db_adapter.curw_sim.grids import add_flo2d_initial_conditions, get_flo2d_initial_conditions, \
+    clear_initial_conditions
 from db_adapter.constants import CURW_SIM_HOST, CURW_SIM_PORT, CURW_SIM_USERNAME, CURW_SIM_PASSWORD, CURW_SIM_DATABASE
 from db_adapter.curw_sim.constants import FLO2D_250, FLO2D_150
+
+flo2d_model_list = [FLO2D_250, FLO2D_150]
 
 print(" Add flo2d initial conditions")
 
@@ -10,18 +13,15 @@ try:
 
     pool = get_Pool(host=CURW_SIM_HOST, port=CURW_SIM_PORT, user=CURW_SIM_USERNAME, password=CURW_SIM_PASSWORD, db=CURW_SIM_DATABASE)
 
-    print("Add flo2d 250 initial conditions")
-    add_flo2d_initial_conditions(pool=pool, flo2d_model=FLO2D_250,
-                                 initial_condition_file_path='grid_maps/flo2d/initial_conditions/{}_initial_cond.csv'
-                                 .format(FLO2D_250))
-    print("{} initial conditions added".format(len(get_flo2d_initial_conditions(pool=pool, flo2d_model=FLO2D_250))))
+    for flo2d_model in flo2d_model_list:
+        print("Clear existing {} initial conditions".format(flo2d_model))
+        print("{} initial conditions cleared.".format(clear_initial_conditions(pool=pool, flo2d_model=flo2d_model)))
 
-    print("Add flo2d 150 initial conditions")
-    add_flo2d_initial_conditions(pool=pool, flo2d_model=FLO2D_150,
-                                 initial_condition_file_path='grid_maps/flo2d/initial_conditions/{}_initial_cond.csv'
-                                 .format(FLO2D_150))
-    print("{} initial conditions added".format(len(get_flo2d_initial_conditions(pool=pool, flo2d_model=FLO2D_150))))
-
+        print("Add {} initial conditions".format(flo2d_model))
+        add_flo2d_initial_conditions(pool=pool, flo2d_model=flo2d_model,
+                                     initial_condition_file_path='grid_maps/flo2d/initial_conditions/{}_initial_cond.csv'
+                                     .format(flo2d_model))
+        print("{} initial conditions added".format(len(get_flo2d_initial_conditions(pool=pool, flo2d_model=flo2d_model))))
 
 except Exception as e:
     traceback.print_exc()
