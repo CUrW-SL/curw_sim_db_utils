04d977dbfb85b21de3b5f646e53a342063f4da60
shadhini <jshadhiniaseka@gmail.com>
2019-10-30 10:18:23 +0530

    add function to retrive set of hash ids from curw_sim run tables according to given conditions

--- flush_data/flush_older_data.py
+++ flush_data/flush_older_data.py
@@ -79,3 +79,70 @@ class Timeseries:
         finally:
             if connection is not None:
                 connection.close()
+
+
+def get_curw_sim_hash_ids(pool, run_table, model=None, method=None, obs_end_start=None, obs_end_end=None, grid_id=None):
+
+    """
+    Retrieve specific set of hash ids from curw_sim run tables
+    :param pool: database connection pool
+    :param model: target model
+    :param method: interpolation method
+    :param obs_end_start: start of the considering obs_end range, inclusive
+    :param obs_end_end: end of the considering obs_end range, inclusive
+    :param grid_id: grid id pattern, escape character $
+    :return:
+    """
+
+    pre_sql_statement = "SELECT `id` FROM `" + run_table + "` WHERE "
+
+    condition_list = []
+    variable_list = []
+
+    score = 0
+
+    if model is not None:
+        condition_list.append("`model`=%s")
+        variable_list.append(model)
+        score +=1
+    if method is not None:
+        condition_list.append("`method`=%s")
+        variable_list.append(method)
+        score +=1
+    if obs_end_start is not None:
+        condition_list.append("`obs_end`>=%s")
+        variable_list.append(obs_end_start)
+        score +=1
+    if obs_end_end is not None:
+        condition_list.append("`obs_end`<=%s")
+        variable_list.append(obs_end_end)
+        score +=1
+    if grid_id is not None:
+        condition_list.append("`grid_id` like %s ESCAPE '$'")
+        variable_list.append(grid_id)
+        score +=1
+
+    if score == 0:
+        return None
+
+    conditions = " AND ".join(condition_list)
+
+    sql_statement = pre_sql_statement + conditions + ";"
+
+    print(sql_statement)
+
+    ids = []
+    connection = pool.connection()
+    try:
+        with connection.cursor() as cursor:
+            row_count = cursor.execute(sql_statement, tuple(variable_list))
+            if row_count > 0:
+                results = cursor.fetchall()
+                for result in results:
+                    ids.append(result.get('id'))
+        return ids
+    except Exception:
+        traceback.print_exc()
+    finally:
+        if connection is not None:
+            connection.close()
