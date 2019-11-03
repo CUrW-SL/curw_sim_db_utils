fd75f948af723c75b59884fe733741884a56afd7
shadhini <jshadhiniaseka@gmail.com>
2019-11-04 01:00:10 +0530

    add scripts to push flo2d 150 rfields to google bucket

---
+++ rain/rfields/push_flo2d_150_rfields_to_bucket.py
@@ -0,0 +1,68 @@
+import traceback
+import os
+import json
+from datetime import datetime, timedelta, date
+
+from db_adapter.constants import COMMON_DATE_TIME_FORMAT
+from db_adapter.curw_sim.constants import FLO2D_150
+from db_adapter.curw_sim.grids import GridInterpolationEnum
+from db_adapter.curw_sim.timeseries import MethodEnum
+
+FLO2D_150_RFIELD_DIR = "rain/rfields/flo2d_150"
+FLO2D_150_RFIELD_BUCKET_DIR = "/mnt/disks/wrf_nfs/flo2d_raincells/150/rfield"
+FORWARD = 1
+BACKWARD = 5
+
+
+def read_attribute_from_config_file(attribute, config, compulsory):
+    """
+    :param attribute: key name of the config json file
+    :param config: loaded json file
+    :param compulsory: Boolean value: whether the attribute is must present or not in the config file
+    :return:
+    """
+    if attribute in config and (config[attribute]!=""):
+        return config[attribute]
+    elif compulsory:
+        print("{} not specified in config file.".format(attribute))
+        exit(1)
+    else:
+        return None
+
+
+if __name__=="__main__":
+    try:
+
+        config = json.loads(open('rain/rfields/150_config.json').read())
+
+        # date
+        day = read_attribute_from_config_file('day', config, False)
+
+        # source details
+        method = MethodEnum.getAbbreviation(MethodEnum.MME)
+        grid_interpolation = GridInterpolationEnum.getAbbreviation(GridInterpolationEnum.MDPA)
+
+        if day:
+            day_0 = (datetime.strptime(day, "%Y-%m-%d")).date()
+        else:
+            day_0 = (datetime.now() + timedelta(hours=5, minutes=30)).date()
+
+        start = day_0 - timedelta(days=BACKWARD)
+        end = day_0 + timedelta(days=FORWARD)
+
+        delta = end - start  # as timedelta
+
+        rfield_locations = ""
+
+        for i in range(delta.days + 1):
+            day = start + timedelta(days=i)
+            rfield_locations += " {}/{}/*".format(FLO2D_150_RFIELD_DIR, day.strftime("%Y-%m-%d"))
+
+        print("{} : ####### Push FLO2D 150 rfields to google bucket".format(datetime.now()))
+        os.system("tar --transform 's/.*\///g' -czf {}/{}.tar.gz{}".format(FLO2D_150_RFIELD_BUCKET_DIR,
+                                                   day_0.strftime("%Y-%m-%d_%H-%M"), rfield_locations))
+
+    except Exception as e:
+        traceback.print_exc()
+    finally:
+        print("{} : ####### Push {} rfields to google bucket #######".format(datetime.now(), FLO2D_150))
---
+++ rain/rfields/push_flo2d_150_rfields_to_bucket.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+echo `date`
+
+echo "Changing into ~/curw_sim_db_utils"
+cd /home/uwcc-admin/curw_sim_db_utils
+echo "Inside `pwd`"
+
+
+# If no venv (python3 virtual environment) exists, then create one.
+if [ ! -d "venv" ]
+then
+    echo "Creating venv python3 virtual environment."
+    virtualenv -p python3 venv
+fi
+
+# Activate venv.
+echo "Activating venv python3 virtual environment."
+source venv/bin/activate
+
+# Install dependencies using pip.
+if [ ! -f "curw_sim_utils.log" ]
+then
+    echo "Installing PyMySQL"
+    pip install PyMySQL
+    echo "Installing PyYAML"
+    pip install PyYAML
+    echo "Installing db adapter"
+    pip install git+https://github.com/shadhini/curw_db_adapter.git
+    touch curw_sim_utils.log
+fi
+
+
+# Generate flo2d 150 rfields in obs range
+echo "Running push_flo2d_150_rfields_to_bucket.py"
+python rain/rfields/push_flo2d_150_rfields_to_bucket.py >> rain/rfields/curw_sim_push_flo2d_150_rfields_to_bucket.log 2>&1
+
+# Deactivating virtual environment
+echo "Deactivating virtual environment"
+deactivate
