2e38956157ff2bcd599e9dcc6fbd18a2f6dfeb10
shadhini <jshadhiniaseka@gmail.com>
2019-11-04 04:50:48 +0530

    reformat dir path

--- rain/rfields/push_flo2d_150_rfields_to_bucket.py
+++ rain/rfields/push_flo2d_150_rfields_to_bucket.py
@@ -56,7 +56,7 @@ if __name__=="__main__":
 
         for i in range(delta.days + 1):
             day = start + timedelta(days=i)
-            dir_path = "{}/{}/*".format(FLO2D_150_RFIELD_DIR, day.strftime("%Y-%m-%d"))
+            dir_path = "{}/{}".format(FLO2D_150_RFIELD_DIR, day.strftime("%Y-%m-%d"))
             if os.path.isdir(dir_path):
                 rfield_locations += " {}/*".format(dir_path)
 
