f0eb9bc1cd28faf01e105cd29d16cd5f7e0df79b
shadhini <jshadhiniaseka@gmail.com>
2020-01-31 11:28:10 +0530

    set custom config paths for self executable data flushing scripts

--- flush_data/flush_hechms_rain_data.py
+++ flush_data/flush_hechms_rain_data.py
@@ -1,9 +1,12 @@
 #!/home/uwcc-admin/curw_sim_db_utils/venv/bin/python3
 import traceback
+import os
 from datetime import datetime, timedelta
 
 from db_adapter.base import get_Pool, destroy_Pool
-from db_adapter.constants import CURW_SIM_PASSWORD, CURW_SIM_DATABASE, CURW_SIM_USERNAME, CURW_SIM_PORT, CURW_SIM_HOST
+from db_adapter.constants import set_db_config_file_path
+from db_adapter.constants import connection as con_params
+# from db_adapter.constants import CURW_SIM_PASSWORD, CURW_SIM_DATABASE, CURW_SIM_USERNAME, CURW_SIM_PORT, CURW_SIM_HOST
 from db_adapter.constants import COMMON_DATE_TIME_FORMAT
 from db_adapter.curw_sim.constants import HecHMS
 from db_adapter.curw_sim.timeseries import MethodEnum, Timeseries
@@ -13,12 +16,16 @@ import sys
 sys.path.insert(0, '/home/uwcc-admin/flush_data')
 import flush_curw_sim_data_common as flush_common
 
+ROOT_DIRECTORY = '/home/uwcc-admin/curw_sim_db_utils'
+
 if __name__=="__main__":
+
+    set_db_config_file_path(os.path.join(ROOT_DIRECTORY, 'db_adapter_config.json'))
 
     try:
 
-        pool = get_Pool(host=CURW_SIM_HOST, port=CURW_SIM_PORT, user=CURW_SIM_USERNAME, password=CURW_SIM_PASSWORD,
-                        db=CURW_SIM_DATABASE)
+        pool = get_Pool(host=con_params.CURW_SIM_HOST, port=con_params.CURW_SIM_PORT, user=con_params.CURW_SIM_USERNAME,
+                        password=con_params.CURW_SIM_PASSWORD, db=con_params.CURW_SIM_DATABASE)
 
         method = MethodEnum.getAbbreviation(MethodEnum.MME)
         run_table = "run"
