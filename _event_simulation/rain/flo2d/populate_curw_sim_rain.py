8ae2318f9e6f635c53e06413ebac940ca494a8c4
shadhini <jshadhiniaseka@gmail.com>
2020-02-20 18:10:41 +0530

    add function to calculate voronoi polygons

--- _event_simulation/rain/flo2d/populate_curw_sim_rain.py
+++ _event_simulation/rain/flo2d/populate_curw_sim_rain.py
@@ -5,6 +5,12 @@ import pandas as pd
 from datetime import datetime, timedelta
 import os, sys, getopt
 
+import numpy as np
+import geopandas as gpd
+from scipy.spatial import Voronoi
+from shapely.geometry import Polygon, Point
+
+
 from db_adapter.base import get_Pool
 from db_adapter.constants import set_db_config_file_path
 from db_adapter.constants import connection as con_params
@@ -29,6 +35,117 @@ def usage():
     -f  --file_path     Corrected rain file path. e.g.: "/mnt/disks/wrf_nfs/wrf/corrected_rf.csv"
     """
     print(usageText)
+
+
+def _voronoi_finite_polygons_2d(vor, radius=None):
+    """
+    Reconstruct infinite voronoi regions in a 2D diagram to finite
+    regions.
+    Parameters
+    ----------
+    vor : Voronoi
+        Input diagram
+    radius : float, optional
+        Distance to 'points at infinity'.
+    Returns
+    -------
+    regions : list of tuples
+        Indices of vertices in each revised Voronoi regions.
+    vertices : list of tuples
+        Coordinates for revised Voronoi vertices. Same as coordinates
+        of input vertices, with 'points at infinity' appended to the
+        end.
+    from: https://stackoverflow.com/questions/20515554/colorize-voronoi-diagram
+    """
+    if vor.points.shape[1] != 2:
+        raise ValueError("Requires 2D input")
+
+    new_regions = []
+    new_vertices = vor.vertices.tolist()
+    center = vor.points.mean(axis=0)
+    if radius is None:
+        radius = vor.points.ptp().max()
+    # Construct a map containing all ridges for a given point
+    all_ridges = {}
+    for (p1, p2), (v1, v2) in zip(vor.ridge_points, vor.ridge_vertices):
+        all_ridges.setdefault(p1, []).append((p2, v1, v2))
+        all_ridges.setdefault(p2, []).append((p1, v1, v2))
+
+    # Reconstruct infinite regions
+    for p1, region in enumerate(vor.point_region):
+        vertices = vor.regions[region]
+        if all(v >= 0 for v in vertices):
+            # finite region
+            new_regions.append(vertices)
+            continue
+        # reconstruct a non-finite region
+        ridges = all_ridges[p1]
+        new_region = [v for v in vertices if v >= 0]
+
+        for p2, v1, v2 in ridges:
+            if v2 < 0:
+                v1, v2 = v2, v1
+            if v1 >= 0:
+                # finite ridge: already in the region
+                continue
+            # Compute the missing endpoint of an infinite ridge
+            t = vor.points[p2] - vor.points[p1]  # tangent
+            t /= np.linalg.norm(t)
+            n = np.array([-t[1], t[0]])  # normal
+
+            midpoint = vor.points[[p1, p2]].mean(axis=0)
+            direction = np.sign(np.dot(midpoint - center, n)) * n
+            far_point = vor.vertices[v2] + direction * radius
+            new_region.append(len(new_vertices))
+            new_vertices.append(far_point.tolist())
+
+        # sort region counterclockwise
+        vs = np.asarray([new_vertices[v] for v in new_region])
+        c = vs.mean(axis=0)
+        angles = np.arctan2(vs[:, 1] - c[1], vs[:, 0] - c[0])
+        new_region = np.array(new_region)[np.argsort(angles)]
+        # finish
+        new_regions.append(new_region.tolist())
+    return new_regions, np.asarray(new_vertices)
+
+
+def get_voronoi_polygons(points_dict, shape_file, shape_attribute=None, output_shape_file=None, add_total_area=True):
+    """
+    :param points_dict: dict of points {'id' --> [lon, lat]}
+    :param shape_file: shape file path of the area
+    :param shape_attribute: attribute list of the interested region [key, value]
+    :param output_shape_file: if not none, a shape file will be created with the output
+    :param add_total_area: if true, total area shape will also be added to output
+    :return:
+    geo_dataframe with voronoi polygons with columns ['id', 'lon', 'lat','area', 'geometry'] with last row being the area of the
+    shape file
+    """
+    if shape_attribute is None:
+        shape_attribute = ['OBJECTID', 1]
+
+    shape_df = gpd.GeoDataFrame.from_file(shape_file)
+    shape_polygon_idx = shape_df.index[shape_df[shape_attribute[0]] == shape_attribute[1]][0]
+    shape_polygon = shape_df['geometry'][shape_polygon_idx]
+
+    ids = [p if type(p) == str else np.asscalar(p) for p in points_dict.keys()]
+    points = np.array(list(points_dict.values()))[:, :2]
+    vor = Voronoi(points)
+
+    regions, vertices = _voronoi_finite_polygons_2d(vor)
+
+    data = []
+    for i, region in enumerate(regions):
+        polygon = Polygon([tuple(x) for x in vertices[region]])
+        if polygon.intersects(shape_polygon):
+            intersection = polygon.intersection(shape_polygon)
+            data.append({'id': ids[i], 'lon': vor.points[i][0], 'lat': vor.points[i][1], 'area': intersection.area,
+                         'geometry': intersection
+                         })
+    df = gpd.GeoDataFrame(data, columns=['id', 'lon', 'lat', 'area', 'geometry'], crs=shape_df.crs)
+    if output_shape_file is not None:
+        df.to_file(output_shape_file)
+
+    return df
 
 
 if __name__=="__main__":
@@ -82,11 +199,17 @@ if __name__=="__main__":
                         db=con_params.CURW_SIM_DATABASE,
                         user=con_params.CURW_SIM_USERNAME, password=con_params.CURW_SIM_PASSWORD)
 
-        print(corrected_rf_df)
+        points_dict = {}
         print('distinct_stations')
-        print(distinct_stations.size)
+        count=1000
+        for index, row in distinct_stations.iteritems():
+            print(index[0], index[1])
+
+
 
     except Exception as e:
         traceback.print_exc()
     finally:
         print("Process finished.")
+
+
