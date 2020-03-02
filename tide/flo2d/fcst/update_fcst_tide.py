be02549d543d778a0eafb26a8370b63042ab373e
shadhini <jshadhiniaseka@gmail.com>
2020-03-02 15:34:21 +0530

    debug database insertion format issue

--- tide/flo2d/fcst/update_fcst_tide.py
+++ tide/flo2d/fcst/update_fcst_tide.py
@@ -78,13 +78,15 @@ def process_tide_fcsts_from_Mobile_Geographics(existing_ts_end, obs_end):
 
         hourly_ts_df = timeseries_df.resample('H').asfreq()
         hourly_ts_df.fillna(-99999.000)
+        hourly_ts_df = hourly_ts_df.reset_index()
+
         pd.set_option('display.max_rows', hourly_ts_df.shape[0] + 1)
         pd.set_option('display.max_columns', hourly_ts_df.shape[1] + 1)
-        hourly_ts_df = hourly_ts_df.reset_index()
         hourly_ts_df = pd.to_datetime(hourly_ts_df['time'], format=COMMON_DATE_TIME_FORMAT)
 
         print(hourly_ts_df)
         processed_timeseries = hourly_ts_df.values.tolist()
+        print(processed_timeseries)
 
         return processed_timeseries
     else:
