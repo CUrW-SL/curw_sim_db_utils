d8fb04bb83df0914dd6a0d7dacad6c4cce8326ef
shadhini <jshadhiniaseka@gmail.com>
2020-06-11 14:37:32 +0530

    linearly interpolate tidal timeseries to fill missing values

--- tide/flo2d/fcst/update_fcst_tide.py
+++ tide/flo2d/fcst/update_fcst_tide.py
@@ -80,7 +80,7 @@ def process_tide_fcsts_from_Mobile_Geographics(existing_ts_end, obs_end):
         timeseries_df.set_index('time', inplace=True)
 
         hourly_ts_df = timeseries_df.resample('H').asfreq()
-        hourly_ts_df = hourly_ts_df.to_numeric()
+        hourly_ts_df = pd.to_numeric(hourly_ts_df)
         hourly_ts_df = hourly_ts_df.interpolate(method='linear', limit_direction='both', limit=100) ####temp###
         hourly_ts_df = hourly_ts_df.fillna(-99999.000)
         hourly_ts_df.index = hourly_ts_df.index.map(str)
